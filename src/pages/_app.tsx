48f8f921dd88f321789fbb710e3442a369d37ff6
Vinicius Uehara <viuehara12@gmail.com>
2021-07-06 13:43:49 -0300

    chore: add favicon, title and site description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=0.86" />
+        <meta
+          name="description"
+          content="Com o Adiante é possível. Antecipe os recebíveis da sua empresa de forma rápida e segura e obtenha fluxo de caixa ainda hoje. Cadastro simples e 100% online. "
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Adiante S/A</title>
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
